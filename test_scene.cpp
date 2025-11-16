ed02498a6420ffc1310853f201043ebb0bccc5ab
Hayoruni <kristof1690@gmail.com>
2025-11-16 01:00:14 +0100

    1 feladat?
    talán kész de nem vagyok benne biztos hogy thread safe

--- test_scene.cpp
+++ test_scene.cpp
@@ -1,4 +1,5 @@
 #include "test_scene.h"
+#include <windows.h>
 
 void TestScene::input_event(const Ref<InputEvent> &event){
 
@@ -38,21 +39,54 @@ void TestScene::input_event(const Ref<InputEvent> &event){
 }
 
 void TestScene::GenerateChunk(int x, int y){
-    chunks.push_back(Chunk(x, y));
-    //TODO kiboviteni
+    List<TerrainItem> terrainObjs = {};
+    RandomPCG rnd;
+    rnd.seed(GetTickCount64()+rndStart.random(-9999999,9999999)); //TODO ide lehet kell mutex lock mert nem tudom hogy keri le a kozos randomot de jonak tunik szoval idk
+    int rockCount = rnd.random(0, 4);
+    for(int i = 0; i<rockCount; i++){
+        int rockX = rnd.random(0, 17);
+        int rockY = rnd.random(0, 17);
+        int rockT = rnd.random(0, 3);
+        terrainObjs.push_back(TerrainItem((int)(rockX*(chunkBlockSize/3)+x), (int)(rockY*(chunkBlockSize/3)+y), &_rockTextures[rockT]));
+    }
+
+
+    mtx.lock();
+    chunks.push_back(Chunk(x, y, terrainObjs));
+    mtx.unlock();
+}
+
+void TestScene::thread_func(void *p_user_data){
+    ThreadData* td = reinterpret_cast<ThreadData*>(p_user_data);
+    td->scene->GenerateChunk(td->startX, td->startY);
+    delete td;
 }
 
 void TestScene::update(float delta){
     if(!start){
-        GenerateChunk(0, 0);
-        GenerateChunk(chunkBlockSize*6, 0);
-        GenerateChunk(-chunkBlockSize*6, 0);
-        GenerateChunk(0, chunkBlockSize*6);
-        GenerateChunk(0, -chunkBlockSize*6);
+        Thread t1, t2, t3, t4, t5, t6, t7, t8, t9;
+        ThreadData* td1 = new ThreadData{0, 0, this};
+        ThreadData* td2 = new ThreadData{chunkBlockSize*6, 0, this};
+        ThreadData* td3 = new ThreadData{-chunkBlockSize*6, 0, this};
+        ThreadData* td4 = new ThreadData{0, chunkBlockSize*6, this};
+        ThreadData* td5 = new ThreadData{0, -chunkBlockSize*6, this};
+        ThreadData* td6 = new ThreadData{chunkBlockSize*6, chunkBlockSize*6, this};
+        ThreadData* td7 = new ThreadData{chunkBlockSize*6, -chunkBlockSize*6, this};
+        ThreadData* td8 = new ThreadData{-chunkBlockSize*6, chunkBlockSize*6, this};
+        ThreadData* td9 = new ThreadData{-chunkBlockSize*6, -chunkBlockSize*6, this};
+        t1.start(&TestScene::thread_func, td1);
+        t2.start(&TestScene::thread_func, td2);
+        t3.start(&TestScene::thread_func, td3);
+        t4.start(&TestScene::thread_func, td4);
+        t5.start(&TestScene::thread_func, td5);
+        t6.start(&TestScene::thread_func, td6);
+        t7.start(&TestScene::thread_func, td7);
+        t8.start(&TestScene::thread_func, td8);
+        t9.start(&TestScene::thread_func, td9);
 
         start = true;
     }
-    if(pressW) camY += playerSpeed; //ezeket meg kéne szorozni deltaTime-al de nem tudom hol van
+    if(pressW) camY += playerSpeed; //TODO ezeket meg kéne szorozni deltaTime-al de nem tudom hol van
     if(pressS) camY -= playerSpeed; //és biztos hogy nem igy kell nézni az inputot de most debughoz jó
     if(pressA) camX += playerSpeed;
     if(pressD) camX -= playerSpeed;
@@ -76,12 +110,22 @@ void TestScene::render(){
         y += chunkBlockSize;
         x = chunks[i].x;
         }
+        for(int j = 0;j<chunks[i].terrainItems.size();j++){
+            r->draw_texture(*chunks[i].terrainItems[j].texture, Rect2(chunks[i].terrainItems[j].x + camX, chunks[i].terrainItems[j].y + camY, chunkBlockSize/3, chunkBlockSize/3));
+        }
     }
 
     //debug
     if(showChunkBorder){
         for(int i = 0; i < chunks.size(); i++){
             r->draw_line_rect(Rect2(chunks[i].x+camX, chunks[i].y+camY, chunkBlockSize*6, chunkBlockSize*6),Color(180, 0, 0), 2);
+        }
+    }
+    if(showTerrainItemBorder){
+        for(int i = 0; i < chunks.size(); i++){
+            for(int j = 0;j<chunks[i].terrainItems.size();j++){
+                r->draw_line_rect(Rect2(chunks[i].terrainItems[j].x+camX, chunks[i].terrainItems[j].y+camY, chunkBlockSize/3, chunkBlockSize/3),Color(0, 0, 180), 2);
+            }
         }
     }
 
@@ -102,6 +146,9 @@ void TestScene::render(){
     if(ImGui::Button("Chunk Border on/off")){
         showChunkBorder = !showChunkBorder;
     };
+    if(ImGui::Button("Terrain Item Border on/off")){
+        showTerrainItemBorder = !showTerrainItemBorder;
+    };
     if(pressW) ImGui::Text("W Pressed");
     else ImGui::Text("W Released");
     if(pressS) ImGui::Text("S Pressed");
@@ -121,13 +168,35 @@ TestScene::TestScene(){
 
     _grassImage.instance();
     _grassImage->load_from_file("assetts/inUse/grass.png");
-
     _grassTexture.instance();
     _grassTexture->create_from_image(_grassImage);
+
+    _rockImage1.instance();
+    _rockImage1->load_from_file("assetts/inUse/Rock1.png");
+    _rockTexture1.instance();
+    _rockTexture1->create_from_image(_rockImage1);
+    _rockImage2.instance();
+    _rockImage2->load_from_file("assetts/inUse/Rock2.png");
+    _rockTexture2.instance();
+    _rockTexture2->create_from_image(_rockImage2);
+    _rockImage3.instance();
+    _rockImage3->load_from_file("assetts/inUse/Rock3.png");
+    _rockTexture3.instance();
+    _rockTexture3->create_from_image(_rockImage3);
+    _rockImage4.instance();
+    _rockImage4->load_from_file("assetts/inUse/Rock4.png");
+    _rockTexture4.instance();
+    _rockTexture4->create_from_image(_rockImage4);
+
+    _rockTextures.push_back(_rockTexture1);
+    _rockTextures.push_back(_rockTexture2);
+    _rockTextures.push_back(_rockTexture3);
+    _rockTextures.push_back(_rockTexture4);
 
     chunks = {};
     camX = 0;
     camY = 0;
     playerSpeed = 20;
     chunkBlockSize = 200;
+    rndStart.seed(GetTickCount64());
 }
--- test_scene.h
+++ test_scene.h
@@ -2,17 +2,28 @@
 #define TEST_SCENE_H
 
 #include "sfw.h"
+struct TerrainItem{
+    int x = 0;
+    int y = 0;
+    Ref<Texture>* texture;
 
-class Chunk{ //egy chunk 6x6 darab 200 x 200 pixel-es részbõl van
-
-public:
-	int x = 0;
-	int y = 0;
-
-    Chunk() {}
-	Chunk(int x, int y){
+    TerrainItem(){};
+    TerrainItem(int x, int y, Ref<Texture>* texture){
         this->x = x;
         this->y = y;
+        this->texture = texture;
+	}
+};
+struct Chunk{ //egy chunk 6x6 darab 200 x 200 pixel-es részbõl van
+    int x = 0;
+	int y = 0;
+	List<TerrainItem> terrainItems = {};
+
+    Chunk() {}
+	Chunk(int x, int y, List<TerrainItem> terrainItems){
+        this->x = x;
+        this->y = y;
+        this->terrainItems = terrainItems;
 	}
 };
 
@@ -23,16 +34,19 @@ public:
 	virtual void input_event(const Ref<InputEvent> &event);
 	virtual void update(float delta);
 	virtual void render();
-	void GenerateChunk(int startX, int startY);
+	virtual void GenerateChunk(int startX, int startY);
+	static void thread_func(void *p_user_data);
 
     TestScene();
 
+    Mutex mtx;
     List<Chunk> chunks;
     int camX;
     int camY;
     int playerSpeed;
     int chunkBlockSize;
     bool start = false; //lefutott-e az update elején egyszer egy start, ez csak most kell talán késöbb nem
+    RandomPCG rndStart;
 
     bool pressW = false;
     bool pressS = false;
@@ -41,10 +55,29 @@ public:
 
     //debug
     bool showChunkBorder = false;
+    bool showTerrainItemBorder = false;
 
 protected:
     Ref<Font> _font;
     Ref<Image> _grassImage;
     Ref<Texture> _grassTexture;
+    Ref<Image> _rockImage1;
+    Ref<Texture> _rockTexture1;
+    Ref<Image> _rockImage2;
+    Ref<Texture> _rockTexture2;
+    Ref<Image> _rockImage3;
+    Ref<Texture> _rockTexture3;
+    Ref<Image> _rockImage4;
+    Ref<Texture> _rockTexture4;
+
+    List<Ref<Texture>> _rockTextures;
+
+};
+
+
+struct ThreadData {
+    int startX;
+    int startY;
+    TestScene* scene;
 };
 #endif
